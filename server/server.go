29ee5ede6319c1d4a38834f49ea45be99cd7cc84
BlueLyo <leandro.cerna@sansano.usm.cl>
2020-12-01 08:53:24 -0300

    ftp y log semilisto

--- server/server.go
+++ server/server.go
@@ -3,9 +3,10 @@ package server
 import (
 	"Tarea2/pb"
 	"fmt"
-	"google.golang.org/grpc"
 	"net"
 	"os"
+
+	"google.golang.org/grpc"
 )
 
 func Run(int_eleccion int8, algoritmo int8) {
@@ -27,31 +28,100 @@ func Run(int_eleccion int8, algoritmo int8) {
 }
 
 func DataNode(algoritmo int8) {
-	lis, err := net.Listen("tcp", ":9000")
+	//generar conexiones a otros DataNodes y NameNode
+	var connlog1 *grpc.ClientConn
 
+	var ftps [3]pb.FTPClient
+	var connftp1 *grpc.ClientConn
+	var connftp2 *grpc.ClientConn
+	var connftp3 *grpc.ClientConn
+
+	//NameNode Conexion
+	connlog1, err := grpc.Dial("dist41:9000", grpc.WithInsecure())
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	logs := pb.NewLOGClient(conn)
+	//DataNode 1  dist42 conexion
+	connlog1, err := grpc.Dial("dist42:9000", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	ftps[0] = pb.NewFTPClient(conn)
+	//DataNode 2  dist43 conexion
+	connlog1, err := grpc.Dial("dist43:9000", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	ftps[1] = pb.NewFTPClient(conn)
+	//DataNode 3  dist44 conexion
+	connlog1, err := grpc.Dial("dist44:9000", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	ftps[2] = pb.NewFTPClient(conn)
 
-	L := pb.DataNode{}
+	//Crear server DataNode puerto 9000
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	//incializar servidor con las conexiones antes creadas
+	L := pb.DataNode{log: logs, Clientes: ftps}
 	Server1 := grpc.NewServer()
 	pb.RegisterFTPServer(Server1, &L)
-
 	if err := Server1.Serve(lis); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	//for para agarrar todos los chunk
-
-	//Luego de tener todos los chunk debe enviarle una propuesta a alguien
-	//Si algoritmo == 1 entonces debe enviarle la propuesta al NameNode
-	//Si algoritmo == 2 entonces debe enviarle la propuesta al resto de DataNodes (1)
-
-	//luego de tener una propuesta aceptada debe enviar los chunks a sus respectivos dueños
 }
 
 func NameNode(algoritmo int8) {
-	//Si algoritmo != 1 entonces no es necesario que reciba propuestas, solo escrituras al log
+	//generar conexiones a otros DataNodes
+	var ftps [3]pb.FTPClient
+	var connftp1 *grpc.ClientConn
+	var connftp2 *grpc.ClientConn
+	var connftp3 *grpc.ClientConn
+	//DataNode 1  dist42 conexion
+	connlog1, err := grpc.Dial("dist42:9000", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	ftps[0] = pb.NewFTPClient(conn)
+	//DataNode 2  dist43 conexion
+	connlog1, err := grpc.Dial("dist43:9000", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	ftps[1] = pb.NewFTPClient(conn)
+	//DataNode 3  dist44 conexion
+	connlog1, err := grpc.Dial("dist44:9000", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	ftps[2] = pb.NewFTPClient(conn)
+
+	//Crear server NameNode puerto 9000
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	//incializar servidor con las conexiones antes creadas
+	L := pb.DataNode{Clientes: ftps}
+	Server1 := grpc.NewServer()
+	pb.RegisterFTPServer(Server1, &L)
+	if err := Server1.Serve(lis); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
